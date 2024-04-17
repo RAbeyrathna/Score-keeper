8bad47243eee7596afbd6b6ebd4e1286ac4b2877
RAbeyrathna <rahal@res-network.com>
2024-04-17 15:49:40 +1000

    refactor: Convert player variables into objects and score keeping into a function

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
+const player1 = {
+    score: 0,
+    button: document.querySelector("#p1Button"),
+    currentScore: document.querySelector("#p1Score")
+}
 
-let p1Score = 0;
-let p2Score = 0;
+const player2 = {
+    score: 0,
+    button: document.querySelector("#p2Button"),
+    currentScore: document.querySelector("#p2Score")
+}
+function updateScores(player, opponent){
+    if(!isGameOver){
+        player.score += 1;      
+        player.currentScore.textContent = player.score;
 
-let p1CurrentScore = document.querySelector("#p1Score");
-let p2CurrentScore = document.querySelector("#p2Score");
+        if(player.score === winningScore){
+            player.currentScore.classList.add("has-text-success");
+            opponent.currentScore.classList.add("has-text-danger");
+            isGameOver = true;
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+    }
+}
+
 let scoreSelect = document.querySelector("#winningScore");
 let resetButton = document.querySelector("#resetButton");
 
@@ -13,12 +31,12 @@ let winningScore = parseInt(scoreSelect.value);
 let isGameOver = false;
 
 const resetScores = () => {
-    p1Score = 0;
-    p2Score = 0;
-    p1CurrentScore.textContent = 0;
-    p2CurrentScore.textContent = 0;
-    p1CurrentScore.classList.remove("has-text-success", "has-text-danger");
-    p2CurrentScore.classList.remove("has-text-success", "has-text-danger");
+    for(let player of [player1,player2]){
+        player.score = 0;
+        player.currentScore.textContent = 0;
+        player.currentScore.classList.remove("has-text-success", "has-text-danger");
+        player.button.disabled = false;
+    }
     isGameOver = false;
 }
 
@@ -29,31 +47,12 @@ scoreSelect.addEventListener('change', function() {
 );
 
 
-p1Button.addEventListener('click', () => {
-    if(!isGameOver){
-        p1Score += 1;      
-        p1CurrentScore.textContent = p1Score;
-
-        if(p1Score === winningScore){
-            p1CurrentScore.classList.add("has-text-success");
-            p2CurrentScore.classList.add("has-text-danger");
-            isGameOver = true;
-        }
-    }
+player1.button.addEventListener('click', () => {
+    updateScores(player1,player2);
 });
 
-p2Button.addEventListener('click', () => {
-    if(!isGameOver){
-        p2Score += 1;      
-        p2CurrentScore.textContent = p2Score;
-
-        if(p2Score === winningScore){
-            p1CurrentScore.classList.add("has-text-danger");
-            p2CurrentScore.classList.add("has-text-success");
-
-            isGameOver = true;
-        }
-    }
+player2.button.addEventListener('click', () => {
+    updateScores(player2,player1);
 });
 
 resetButton.addEventListener('click', resetScores);
